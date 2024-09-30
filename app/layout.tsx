845799439335540d22350e985e2fcfbfa417bab5
Fahad <fahadkhann0204@gmail.com>
2024-09-30 15:06:46 +0530

    analytics and speedinsight from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import type { Metadata } from "next";
 import MainNav from "@/app/components/MainNav";
 import TopNavigation from "@/app/components/TopNavigation";
 import Footer from "@/app/components/Footer";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 export const metadata: Metadata = {
@@ -28,6 +30,8 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Inter:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body>
+        <Analytics />
+        <SpeedInsights />
         <TopNavigation />
         <MainNav />
         {children}
